2e9a331e771fc34495559b3ed638707e8d24da9d
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2026-04-01 11:48:59 +0530

    file serving project - lecture

--- ExpressJS/server_01/index.js
+++ ExpressJS/server_01/index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const path = require("path")
+const fs = require("fs")
+const users = require("./users.json")
 
 const app = express()
 
@@ -19,14 +21,30 @@ app.get("/home/form", (req, res) => {
     res.sendFile(RegistrationPageFileLocation)
 })
 
+const SubmitPageFileLocation = path.join(__dirname, "/views/SubmittedPage/index.html")
+
 app.get("/home/submit", (req, res) => {
     res.redirect("/home/form")
 })
 app.post("/home/submit", (req, res) => {
+    users.push({
+        "msg" : "New user added"
+    })
+    fs.writeFile("users.json", JSON.stringify(users), (err, data) => {
+        if(err) console.log(err)
+    })
     const newUser = req.body
-    res.json({success : true,
-        msg : "User added successfully",
-        newUser
+    console.log(newUser)
+    fs.readFile(SubmitPageFileLocation, "utf-8", (err, data) => {
+        if(err) console.log(err)
+            else{
+            const newData = data
+            .replace("{firstName}", newUser.firstName)
+            .replace("{lastName}", newUser.lastName)
+            .replace("{email}", newUser.email)
+            .replace("{title}", newUser.jobTitle)
+            res.send(newData)
+        }
     })
 })
 
